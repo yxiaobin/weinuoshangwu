daeedf922389be1506db30338dd101b202a21294
Your Name <you@example.com>
2019-02-25 15:40:24 +0800

    edit private phone

--- resources/views/home/private.blade.php
+++ resources/views/home/private.blade.php
@@ -848,7 +848,7 @@
         <div class="f_wrap">
             <ul class="clearfix">
                 <li style="margin-right: 134px;margin-left: 140px;">
-                    <span class="mobile">卢经理：021-58690080  180173870022</span>
+                    <span class="mobile">卢经理：021-58690080  18017387002</span>
                 </li>
                 <li style="margin-right: 205px;">
                     <a style="color: #fff;" target="_blank" href="http://p.qiao.baidu.com/cps2/chatIndex?reqParam=%7B%22from%22%3A0%2C%22sid%22%3A%22-100%22%2C%22tid%22%3A%22-1%22%2C%22ttype%22%3A1%2C%22siteId%22%3A%2211894228%22%2C%22userId%22%3A%227727567%22%2C%22pageId%22%3A0%7D" class="online">联系在线客服</a>
