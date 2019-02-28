a528055c3887537c7410394d4ed820632e4d39ff
athicktoplwh <athicktoplwh@126.com>
2019-02-28 16:19:57 +0800

    11

--- resources/views/home/investment.blade.php
+++ resources/views/home/investment.blade.php
@@ -592,6 +592,7 @@
                 <li style="margin-right: 134px;margin-left: 140px;">
                     <span class="mobile">卢经理：021-58690080  18017387002</span>
                 </li>
+                <img src="{{asset('images/1.png')}}" alt="" style="width: 50px;height: 50px;">
                 <li style="margin-right: 205px;">
                     <a style="color: #fff;" target="_blank" href="http://p.qiao.baidu.com/cps2/chatIndex?reqParam=%7B%22from%22%3A0%2C%22sid%22%3A%22-100%22%2C%22tid%22%3A%22-1%22%2C%22ttype%22%3A1%2C%22siteId%22%3A%2211894228%22%2C%22userId%22%3A%227727567%22%2C%22pageId%22%3A0%7D" class="online">联系在线客服</a>
                 </li>
